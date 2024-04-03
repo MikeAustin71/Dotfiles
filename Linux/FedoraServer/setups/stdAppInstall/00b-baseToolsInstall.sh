374ffb1a694776f9f96d4abcbada08e42201b101
Mike Rapp <jordy.rascal@protonmail.com>
2024-04-03 02:58:59 -0500

    Added fonts and wallpaper installers to script 00b-baseToolsInstall.sh.

--- Linux/FedoraServer/setups/stdAppInstall/00b-baseToolsInstall.sh
+++ Linux/FedoraServer/setups/stdAppInstall/00b-baseToolsInstall.sh
@@ -1,11 +1,33 @@
 #!/bin/bash
 # Installs base Tools
 # Run this as sudo
+# NOTE: This script will fail if you
+#   did NOT reboot your computer after
+#   running script 00a-createBaseStructure.sh
 
 
 source "$MIKE_Setup_Utilities"/utilsLib.sh
 source "$MIKE_Setup_Utilities"/utilsAppInstall.sh
 
+function getFonts() {
+
+  local scriptFile
+
+     scriptFile="$MIKE_Setup_Scripts"/cfgFonts/getAllFonts.sh
+
+  # shellcheck disable=SC1090
+  source "$scriptFile"
+}
+
+function getWallpaper() {
+
+  local scriptFile
+
+     scriptFile="$MIKE_Setup_Scripts"/copyWallpaper/copyWallpaperFromDotFiles.sh
+
+  # shellcheck disable=SC1090
+  source "$scriptFile"
+}
 
 installGit &&
 installCoreUtils &&
@@ -16,4 +38,6 @@ installCompressionUtilities &&
 installNano &&
 installNeofetch &&
 installVmwareTools &&
-successMsg "All Base Tools Installed."
+getFonts &&
+getWallpaper &&
+successMsg "All Base Tools Installed." "All Fonts Installed" "All Wallpapers Installed"
