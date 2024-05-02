74daa9a1c3db7b674a98ec67cd8ffc49d5e2dae7
Mike Rapp <jordy.rascal@protonmail.com>
2024-05-02 04:44:37 -0500

    Refactored saveConfigsToVM.sh.

--- Linux/FedoraServer/setups/customAppConfig/saveConfigsToVM.sh
+++ Linux/FedoraServer/setups/customAppConfig/saveConfigsToVM.sh
@@ -8,55 +8,66 @@
 source "$MIKE_Setup_Utilities"/utilsLib.sh
 
 
-  declare -i saveConfigsToVM_ErrorCode=0
+ declare -i saveConfigsToVM_ErrorCode=0
 
+ declare vmShareTargetBaseDir="$MIKE_VMShared_Drive"/fedora_server/final_production
+
+ declare vmShareTargetCfgDir="$vmShareTargetBaseDir"/.config
+
+
+function deleteHomeConfigVMShare() {
+
+  [[ -d $vmShareTargetBaseDir ]] || {
+
+    saveConfigsToVM_ErrorCode=199
+
+    errXMsg "VM Target Base Directory Does NOT Exist!" "$vmShareTargetBaseDir" "Function: deleteHomeConfigVMShare" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCodee"
+
+    return $saveConfigsToVM_ErrorCode
+  }
+
+  [[ ! -d $vmShareTargetCfgDir ]] || {
+
+    zapFilesCmd "$vmShareTargetCfgDir" "-rfv" "" || {
+
+      saveConfigsToVM_ErrorCode=$?
+
+      errXMsg "Attempted deletion of target VM Directory Failed" "$vmShareTargetCfgDir" "Function: deleteHomeConfigVMShare" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCode"
+
+      return $saveConfigsToVM_ErrorCode
+
+    }
+
+    echo
+    echo "Successfully Deleted VM Config Directory:"
+    echo "$vmShareTargetCfgDir"
+    echo "Function: deleteHomeConfigVMShare"
+    echo "Script: saveConfigsToVM.sh"
+    echo
+  }
+
+  return 0
+}
 
 function copyHomeConfigToVMShare() {
-
-  local targetBaseDir="$MIKE_VMShared_Drive"/fedora_server/final_production
-
-  local targetDir="$targetBaseDir"/.config
 
   local sourceDir="/home/mike/.config"
 
 
    [[ -d $sourceDir ]] || {
 
-    saveConfigsToVM_ErrorCode=199
-
-    errXMsg "Copy Source Directory Does NOT Exist!" "$sourceDir" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCode"
-
-    return $saveConfigsToVM_ErrorCode
-  }
-
-  [[ -d $targetBaseDir ]] || {
-
     saveConfigsToVM_ErrorCode=198
 
-    errXMsg "VM Target Base Directory Does NOT Exist!" "$targetBaseDir" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCodee"
+    errXMsg "Copy Source Directory Does NOT Exist!" "$sourceDir" "Function: copyHomeConfigToVMShare" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCode"
 
     return $saveConfigsToVM_ErrorCode
   }
 
-  [[ ! -d $targetDir ]] || {
-
-    zapFilesCmd "$targetDir" "-rfv" "" || {
+  cp -rv "$sourceDir" "$vmShareTargetBaseDir" || {
 
       saveConfigsToVM_ErrorCode=$?
 
-      errXMsg "Attempted deletion of target VM Directory Failed" "$targetDir" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCode"
-
-      return $saveConfigsToVM_ErrorCode
-
-    }
-
-  }
-
-  cp -rv "$sourceDir" "$targetBaseDir" || {
-
-      saveConfigsToVM_ErrorCode=$?
-
-      errXMsg "Copy Operation Failed" "Source Dir: $sourceDir" "Target Base Dir: $targetBaseDir" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCode"
+      errXMsg "Copy Operation Failed" "Source Dir: $sourceDir" "Target Base Dir: $vmShareTargetBaseDir" "Function: copyHomeConfigToVMShare" "Script: saveConfigsToVM.sh" "Error Code: $saveConfigsToVM_ErrorCode"
 
       return $saveConfigsToVM_ErrorCode
 
@@ -65,7 +76,7 @@ function copyHomeConfigToVMShare() {
   return 0
 }
 
-
+deleteHomeConfigVMShare &&
 copyHomeConfigToVMShare &&
 successMsg "saveConfigsToVM.sh - Successful Completion!" || {
 
