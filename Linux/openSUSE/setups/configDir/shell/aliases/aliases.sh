6e29a622ed387d0aaec650af15aca3b3aad7f97a
Mike Rapp <jordy.rascal@protonmail.com>
2024-05-07 23:30:29 -0500

    Commented out contingent aliases in aliases.sh.

--- Linux/openSUSE/setups/configDir/shell/aliases/aliases.sh
+++ Linux/openSUSE/setups/configDir/shell/aliases/aliases.sh
@@ -17,23 +17,25 @@ alias pat='bat -paging=always'
 # https://github.com/eza-community/eza
 # =======================================
 
+# Uncomment these aliases, only after
+# eza has been installed
 # ld - lists only directories (no files)
-alias ld='eza -lD'
+# alias ld='eza -lD'
 
 # lf - lists only files (no directories)
-alias lf='eza -lF --color=always | grep -v /'
+# alias lf='eza -lF --color=always | grep -v /'
 
 # lh - lists only hidden files (no directories)
-alias lh='eza -al .* --group-directories-first'
+# alias lh='eza -al .* --group-directories-first'
 
 # ls - lists everything with directories first
-alias ls='eza -al --group-directories-first'
+# alias ls='eza -al --group-directories-first'
 
 # lfs - lists only files sorted by size
-alias lfs='eza -alF --color=always --sort=size | grep -v /'
+# alias lfs='eza -alF --color=always --sort=size | grep -v /'
 
 # lt - lists everything sorted by time updated
-alias lt='eza -al --sort=modified'
+# alias lt='eza -al --sort=modified'
 
 # ===================================================
 
@@ -48,7 +50,9 @@ alias tree="br"
 
 alias top="btop"
 
-alias rm="trash-put -iv"
+# Uncomment this alias, only after
+# trash-cli has been installed.
+# alias rm="trash-put -iv"
 
 alias update="sudo zypper update"
 
