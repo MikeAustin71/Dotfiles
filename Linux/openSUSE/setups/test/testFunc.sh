920b9d56c941f0648cf6211ec2d43213f6d1f790
Mike Rapp <jordy.rascal@protonmail.com>
2024-05-18 12:34:34 -0500

    Added error messaging in testFunc.sh.

--- Linux/openSUSE/setups/test/testFunc.sh
+++ Linux/openSUSE/setups/test/testFunc.sh
@@ -1,7 +1,12 @@
 
-source "$HOME"/bashOps/setups/utilsLib.sh
-source "$HOME"/bashOps/setups/utilsAppInstall.sh
-source "$HOME"/bashOps/setups/utilsAppConfig.sh
+source "$HOME"/bashOps/setups/utils/utilsLib.sh
+source "$HOME"/bashOps/setups/utils/utilsAppInstall.sh
+source "$HOME"/bashOps/setups/utils/utilsAppConfig.sh
 
 msgNotify "Installing 'flatseal'" &&
 installFlatSeal &&
+successMsg "'flatseal' successfully installed!" || {
+
+  errXMsg "'flatseal' installation error!"
+
+}
