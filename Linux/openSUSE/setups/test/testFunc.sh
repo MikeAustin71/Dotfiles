1732d857e763660275f8ad4a3903eed3f67eecdd
Mike Rapp <jordy.rascal@protonmail.com>
2024-05-31 00:38:41 -0500

    Refactored test script. testFunc.sh

--- Linux/openSUSE/setups/test/testFunc.sh
+++ Linux/openSUSE/setups/test/testFunc.sh
@@ -1,14 +1,21 @@
 
-source "$HOME"/bashOps/setups/utils/utilsLib.sh
-source "$HOME"/bashOps/setups/utils/utilsAppInstall.sh
-source "$HOME"/bashOps/setups/utils/utilsAppConfig.sh
+declare baseSetups2Dir="$HOME"/bashOps/setups
 
-msgNotify "Installing plocate" &&
-installPlocate &&
-msgNotify "Installing podman" &&
-installPodman &&
-msgNotify "Installing podman Desktop" &&
-installPodman &&
-msgNotify "Installing podman DistroBox" &&
-installDistroBox &&
-successMsg "All Test Applications Installed." "testFunc.sh" || errXMsg "testFunc.sh Execution Failed" "Error-Exit!"
+source "$baseSetups2Dir"/utils/utilsLib.sh
+source "$baseSetups2Dir"/utils/utilsAppConfig.sh
+source "$baseSetups2Dir"/utils/utilsAppInstall.sh
+
+msgNotify "Installing 'zoxide'" &&
+installZoxide &&
+msgNotify "Configuring 'zoxide' in .bashrc" &&
+configBashrcZoxide &&
+msgNotify "Installing Starship" &&
+installStarship &&
+msgNotify "Copying Starship Config Files" &&
+configStarship &&
+msgNotify "Configuring 'starship' in .bashrc" &&
+configBashrcStarship &&
+successMsg "All Phase-2 Structures Created." "REBOOT YOUR COMPUTER!" "Script: 002-createBaseStructure.sh" || {
+
+  errXMsg "002-createBaseStructure.sh Execution Failed" "Error-Exit!"
+}
