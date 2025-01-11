d6bed22271dcc9fe9e69c1c932873c7606e567aa
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-01-10 22:07:46 -0600

    Added wipeSetups.sh

---
+++ Linux/openSUSE/setups/utilsLocal/wipeSetups.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Used to pull down latest version of dotfiles and
+# configure  ~/bashOps/setups
+
+
+declare baseSetups990Dir="$HOME"/bashOps/setups
+
+rm -rf "${baseSetups990Dir:?}/"*
+
