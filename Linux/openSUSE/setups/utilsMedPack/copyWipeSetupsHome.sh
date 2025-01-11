b64731be092fb51660c7e189e7eee4e7af33135f
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-01-11 00:12:56 -0600

    Corrected bash script copyWipeSetupsHome.sh

--- Linux/openSUSE/setups/utilsMedPack/copyWipeSetupsHome.sh
+++ Linux/openSUSE/setups/utilsMedPack/copyWipeSetupsHome.sh
@@ -2,6 +2,9 @@
 # Used to pull down latest version of dotfiles and
 # configure  ~/bashOps/setups
 
+declare baseSetups40Dir="$HOME"/bashOps/setups
+source "$baseSetups40Dir"/utilsLib.sh
+source "$baseSetups40Dir"/utilsAppInstall.sh
 
 
 functionCopyWipeOutHome() {
@@ -20,16 +23,26 @@ local dirOwner
 dirOwner="$(whoami)"
 
 # shellcheck disable=SC1073
- makeDirIfNotExist "$myTargetDir" "$permissionCode" "$dirOwner" || {
+  makeDirIfNotExist "$myTargetDir" "$permissionCode" "$dirOwner" || {
 
     errXMsg "makeDirIfNotExist() Failed with Error" "copyWipeSetupsHome.sh"  "Error-Exit!"
 
     return 99
- }
+  }
 
 
-cp -f "$mySrcFile" "$myTargetFile"
+  cp -f "$mySrcFile" "$myTargetFile" || {
 
+    errXMsg "Copy Error Source: $mySrcFile"  "Destination: $myTargetFile"
 
+  }
 
 }
+
+msgNotify "Copying $HOME/bashOps/setups/utilsLocal/wipeSetups.sh to Secret Sauce" &&
+functionCopyWipeOutHome &&
+successMsg "Copied wipeSetups.sh to Secret Sauce!" "copyWipeSetupsHome.sh" || {
+
+  errXMsg "copyWipeSetupsHome.sh Execution Failed" "Error-Exit!"
+}
+
