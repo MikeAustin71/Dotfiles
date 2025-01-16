b3c6533a1e1d079e43832a11967e7129cf8b9013
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-01-15 20:51:07 -0600

    Refactored path with "$HOME"

--- Linux/openSUSE/setups/configDir/shell/path/userPath.sh
+++ Linux/openSUSE/setups/configDir/shell/path/userPath.sh
@@ -2,14 +2,14 @@
 # Mike's $HOME Directory Path Parameters
 
 # User specific environment
-if ! [[ "$PATH" =~ "$HOME"/go/bin:/usr/local/go:/usr/local/go/bin:/root/.local/bin:"$HOME"/.local/share/fonts ]]
+if ! [[ "$PATH" =~ "$HOME"/go/bin:/usr/local/go:/usr/local/go/bin:/root/.local/bin:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin ]]
+
 then
 
-    PATH="$HOME/go/bin:/usr/local/go:/usr/local/go/bin:/root/.local/bin:$HOME/.local/share/fonts:$PATH"
+    PATH="$HOME"/go/bin:/usr/local/go:/usr/local/go/bin:/root/.local/bin:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin:"$PATH"
 
 fi
 
 export PATH
-
 
 
