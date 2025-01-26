3f70d68adbe21e5aa34aac51b766ed52fc6d9ee5
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-01-26 01:31:27 -0600

    Refacored base setup name in 3b script.

--- Linux/openSUSE/setups/stdAppInstall/003b-utilitiesInstall.sh
+++ Linux/openSUSE/setups/stdAppInstall/003b-utilitiesInstall.sh
@@ -3,11 +3,11 @@
 # Install aliases and starship
 
 
-declare baseSetups9Dir="$HOME"/bashOps/setups
+declare baseSetups3bDir="$HOME"/bashOps/setups
 
-source "$baseSetups9Dir"/utils/utilsLib.sh
-source "$baseSetups9Dir"/utils/utilsAppInstall.sh
-source "$baseSetups9Dir"/utils/utilsAppConfig.sh
+source "$baseSetups3bDir"/utils/utilsLib.sh
+source "$baseSetups3bDir"/utils/utilsAppInstall.sh
+source "$baseSetups3bDir"/utils/utilsAppConfig.sh
 
 msgNotify "Installing 'ghostwriter' markdown editor" &&
 installGhostwriter &&
@@ -26,7 +26,7 @@ installLynis &&
 msgNotify "Installing 'Kate' Text Editor" &&
 installKate &&
 msgNotify "Returning to Home Base" &&
-changeToDir "$baseSetups9Dir/stdAppInstall" &&
+changeToDir "$baseSetups3bDir/stdAppInstall" &&
 successMsg "All Phase-3b Utilities Installed." "003b-utilitiesInstall.sh" || {
 
 errXMsg "003b-utilitiesInstall.sh Execution Failed" "Error-Exit!"
