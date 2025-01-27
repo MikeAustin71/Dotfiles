bfbca82134f628e9305de361744dfd903ea6bbc5
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-01-26 22:39:25 -0600

    Reconfigured user specified path for 'initial' and final path options.

--- Linux/Ubuntu_02/setups/configDir/shell/path/userPath.sh
+++ Linux/Ubuntu_02/setups/configDir/shell/path/userPath.sh
@@ -2,6 +2,8 @@
 # Mike's $HOME Directory Path Parameters
 
 # User specific environment
+
+# Initial Path Spec
 if ! [[ "$PATH" =~ "$HOME"/go/bin:/usr/local/go:/usr/local/go/bin:/root/.local/bin:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin ]]
 
 then
@@ -11,4 +13,20 @@ then
 fi
 
 export PATH
+
+
+
+
+: '
+
+ Final Path Spec
+if ! [[ "$PATH" =~ "$HOME"/go/bin:/usr/local/go:"$HOME"/.cargo:/usr/local/go/bin:/root/.local/bin:"$HOME"/.hishtory:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin ]] ; then
+
+    PATH="$HOME"/go/bin:/usr/local/go:"$HOME"/.cargo:/usr/local/go/bin:/root/.local/bin:"$HOME"/.hishtory:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin:"$PATH"
+
+fi
+
+export PATH
+
+ '
 
--- Linux/openSUSE/setups/configDir/shell/path/userPath.sh
+++ Linux/openSUSE/setups/configDir/shell/path/userPath.sh
@@ -2,13 +2,32 @@
 # Mike's $HOME Directory Path Parameters
 
 # User specific environment
-if ! [[ "$PATH" =~ "$HOME"/go/bin:/usr/local/go:"$HOME"/.cargo/bin:/usr/local/go/bin:/root/.local/bin:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin ]]
+
+
+# Initial Path Spec
+if ! [[ "$PATH" =~ "$HOME"/go/bin:/usr/local/go:/usr/local/go/bin:/root/.local/bin:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin ]]
 
 then
 
-    PATH="$HOME"/go/bin:/usr/local/go:"$HOME"/.cargo/bin:/usr/local/go/bin:/root/.local/bin:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin:"$PATH"
+    PATH="$HOME"/go/bin:/usr/local/go:/usr/local/go/bin:/root/.local/bin:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin:"$PATH"
 
 fi
 
 export PATH
+
+
+
+
+: '
+
+ Final Path Spec
+if ! [[ "$PATH" =~ "$HOME"/go/bin:/usr/local/go:"$HOME"/.cargo:/usr/local/go/bin:/root/.local/bin:"$HOME"/.hishtory:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin ]] ; then
+
+    PATH="$HOME"/go/bin:/usr/local/go:"$HOME"/.cargo:/usr/local/go/bin:/root/.local/bin:"$HOME"/.hishtory:"$HOME"/.config/JetBrains:/usr/share/JetBrains:/opt/JetBrains:"$HOME"/.config/JetBrains/fleet/lib/app/bin/:"$HOME"/.local/share/JetBrains/Toolbox/scripts:"$HOME"/.local/bin:"$HOME"/bin:/usr/local/bin:/usr/bin:/bin:"$PATH"
+
+fi
+
+export PATH
+
+ '
 
