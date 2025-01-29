ae3cf8d156eab3e0e19cfbd99293bfd959707e57
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-01-29 04:29:14 -0600

    Setting Fonts Directory to global fonts directory: /usr/share/fonts

--- Linux/Ubuntu_02/setups/configDir/shell/envars/envars.sh
+++ Linux/Ubuntu_02/setups/configDir/shell/envars/envars.sh
@@ -11,7 +11,7 @@ export XDG_DATA_HOME="$HOME/.local/share"
 
 export XDG_RUNTIME_DIR="/run/user/$UID"
 
-export XDG_DATA_FONTS="$HOME/.local/share/fonts"
+export XDG_DATA_FONTS=/usr/share/fonts/
 
 export XDG_CONFIG_HOME="$HOME/.config"
 
