e39203af77dfa566e287810175038a6ccb1aa39f
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-01-30 23:26:08 -0600

    Refactored C++ Tools installation.

--- Linux/Ubuntu_02/setups/devTools/01-installDevTools.sh
+++ Linux/Ubuntu_02/setups/devTools/01-installDevTools.sh
@@ -2,21 +2,54 @@
 # Installs GNU C and C++ Development Tools
 # https://www.pragmaticlinux.com/2022/01/how-to-install-build-essential-on-opensuse/
 
-echo
-echo "Installing C & C++ Packages"
-echo
+
+  declare -i packDev01ErrCode=0
+
+  echo
+  echo "Installing build-essentials Package"
+  echo
+
+  sudo sudo apt install build-essential || {
+
+    packDev01ErrCode=$?
+
+    echo "Error: 'build-essentials' installation FAILED"
+    echo "Command: sudo apt install build-essential"
+    echo "Script: 01-installDevTools.sh"
+    echo "Error Code: $packDev01ErrCode"
+    return $packDev01ErrCode
+
+  }
+
+  echo
+  echo "Installing manpages-dev Package"
+  echo
+
+
+  sudo apt-get install manpages-dev || {
+
+    packDev01ErrCode=$?
+
+    echo "Error: 'manpages-dev' installation FAILED"
+    echo "Command: sudo apt-get install manpages-dev"
+     echo "Script: 01-installDevTools.sh"
+    echo "Error Code: $packDev01ErrCode"
+    return $packDev01ErrCode
+
+  }
+
+  echo
+  echo "Installing C & C++ Packages"
+  echo
 
   sudo sudo apt install gcc || {
 
-    declare -i theErrCode=0
+    packDev01ErrCode=$?
 
-    theErrCode=$?
-
-    echo "Error 'gcc' install FAILED"
+    echo "Error: 'gcc' installation FAILED"
     echo "Command: sudo sudo apt install gcc"
-    echo "Function: installGCCPlus()"
-    echo "Script: 01-installDevTools.sh"
-    echo "Error Code: $theErrCode"
-    return $theErrCode
+     echo "Script: 01-installDevTools.sh"
+    echo "Error Code: $packDev01ErrCode"
+    return $packDev01ErrCode
 
   }
