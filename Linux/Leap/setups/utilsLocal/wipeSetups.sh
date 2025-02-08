e109355f71ac11370131e1da2dc76a81b9d43ae9
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-02-08 01:49:00 -0600

    Added requrest for user confirmation when deleting the setups directory.

--- Linux/Leap/setups/utilsLocal/wipeSetups.sh
+++ Linux/Leap/setups/utilsLocal/wipeSetups.sh
@@ -3,18 +3,55 @@
 # in $HOME/bashOps/setups
 #   --- BE CAREFUL ---
 
+  declare baseSetups990Dir="$HOME"/bashOps/setups
+  declare homeBase990Dir="$HOME"/secretSauce
+  declare -i wipeSetupsErrCode=0
 
-declare baseSetups990Dir="$HOME"/bashOps/setups
-declare homeBase990Dir="$HOME"/secretSauce
+  echo
+  echo "                       HEADS UP!                               "
+  echo "  *************************************************************"
+  echo "  This bash script will delete and remove the setups directory:"
+  echo "  $baseSetups990Dir"
+  echo "  *************************************************************"
+  echo
+  declare wipePrompt="  Do you want to delete the setups directory (Yy/Nn)?"
+  read answer -p "$wipePrompt" -r -N 1 answer
 
-rm -rf "${baseSetups990Dir:?}/"*
+  if [ "$answer" == "${answer#[Yy]}" ] ;then
+      echo
+      echo "  You did NOT respond 'Yes'."
+      echo "  Therefore, the setups directory WILL NOT BE DELETED."
+      echo "  This script will now exit."
+      echo "  Have a nice day!"
+      echo "  Script File: wipeSetups.sh"
+      echo
 
-cd "$homeBase990Dir" || {
+      exit 0
+  fi
 
-  echo "wipeSetups.sh failed to change directory to home!"
-  echo "home directory = $homeBase990Dir"
+  rm -rf "${baseSetups990Dir:?}/"* ||
+  {
 
-  return 99
+    wipeSetupsErrCode=$?
 
-}
+    echo "  Deletion and Removal of Setups Directory Failed!"
+    echo "  Command:  rm -rf ${baseSetups990Dir:?}/*"
+    echo "  Error Code: $wipeSetupsErrCode"
+    echo "  Script File: wipeSetups.sh"
+    return $wipeSetupsErrCode
+  }
 
+  cd "$homeBase990Dir" || {
+
+    wipeSetupsErrCode=$?
+
+    echo "  wipeSetups.sh failed to change directory to home base!"
+    echo "  home base directory = $homeBase990Dir"
+    echo "  Command: cd $homeBase990Dir"
+    echo "  Error Code: $wipeSetupsErrCode"
+    echo "  Script File: wipeSetups.sh"
+    return $wipeSetupsErrCode
+
+  }
+
+  exit 0
