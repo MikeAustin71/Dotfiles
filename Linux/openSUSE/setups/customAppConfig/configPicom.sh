c67309ca743663bc43acd66d3893250bf1050723
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-02-11 01:30:52 -0600

    Corrected error in picom configuration script

--- Linux/openSUSE/setups/customAppConfig/configPicom.sh
+++ Linux/openSUSE/setups/customAppConfig/configPicom.sh
@@ -1,39 +1,41 @@
 #!/bin/bash
 # Install 'picom' configuration files
 #
+
 declare baseSetPicomDir="$HOME"/bashOps/setups
 source "$baseSetPicomDir"/utils/utilsLib.sh
 source "$baseSetPicomDir"/utils/utilsAppConfig.sh
 source "$baseSetPicomDir"/utils/utilsAppInstall.sh
 
-  declare picomCfgDir="$HOME"/.config/picom
+  declare picomCopyDir="$HOME"/.config
+  declare picomCfgDir="$picomCopyDir"/picom
+  declare picomCfgSrcDir="$baseSetPicomDir"/configDir/picom
   declare picomStartDir
   picomStartDir=$(pwd)
    declare -i picomErrCode=0
 
   function copyPicomCfgFileToLocal() {
 
-    cp -vr "$baseSetPicomDir"/configDir/picom  "$picomCfgDir" || {
+    cp -vr "$picomCfgSrcDir"  "$picomCopyDir" || {
 
 
       picomErrCode=$?
 
-      errXMsg "'picom' Directory Copy Failed" "$baseSetPicomDir/configDir/picom -> $baseSetPicomDir/configDir/picom" "Script: configPicom.sh" "Error Code: $picomErrCode"
+      errXMsg "'picom' Directory Copy Failed" "$picomCfgSrcDir -> $picomCfgDir" "Script: configPicom.sh" "Error Code: $picomErrCode"
 
       return $picomErrCode
 
      }
 
   return 0
+  }
 
-}
-
-msgNotify "Creating 'picom' config directory - if necessary." &&
-makeDirIfNotExist "$picomCfgDir" &&
-msgNotify "Copying 'picom' config files to config directory." &&
-copyPicomCfgFileToLocal &&
-changeToDir "$picomStartDir" &&
-successMsg "Configured 'picom' configuration files in Directory:" "$picomCfgDir" "Script: configPicom.sh" || {
-  picomErrCode=$?
-  errXMsg "Script File configPicom.sh  Execution Failed" "Error Code $picomErrCode" "Script: configPicom.sh"
-}
+  msgNotify "Creating 'picom' config directory - if necessary." &&
+  makeDirIfNotExist "$picomCfgDir" &&
+  msgNotify "Copying 'picom' config files to config directory." &&
+  copyPicomCfgFileToLocal &&
+  changeToDir "$picomStartDir" &&
+  successMsg "Configured 'picom' configuration files in Directory:" "$picomCfgDir" "Script: configPicom.sh" || {
+    picomErrCode=$?
+    errXMsg "Script File configPicom.sh  Execution Failed" "Error Code $picomErrCode" "Script: configPicom.sh"
+  }
