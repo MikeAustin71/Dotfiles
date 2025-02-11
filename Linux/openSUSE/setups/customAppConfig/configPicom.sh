e2a20f2beb9c0a6adaf42852678ced7f48cf4fb1
MikeAustin71MIKE11 <chancy.jordy@protonmail.com>
2025-02-10 23:53:51 -0600

    Updated rofi and picom configurations.

--- Linux/openSUSE/setups/customAppConfig/configPicom.sh
+++ Linux/openSUSE/setups/customAppConfig/configPicom.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
-# This script will initialize and configure
-# Picom in ~/.config/picom
+# Install 'picom' configuration files
+#
+declare baseSetPicomDir="$HOME"/bashOps/setups
+source "$baseSetPicomDir"/utils/utilsLib.sh
+source "$baseSetPicomDir"/utils/utilsAppConfig.sh
+source "$baseSetPicomDir"/utils/utilsAppInstall.sh
 
-source "$MIKE_Setup_Utilities"/utilsLib.sh
+  declare picomCfgDir="$HOME"/.config/picom
+  declare picomStartDir
+  picomStartDir=$(pwd)
+   declare -i picomErrCode=0
 
-declare -i errorCode=0
+  function copyPicomCfgFileToLocal() {
 
-cp -r "$MIKE_Setup_Scripts"/configDir/picom "$XDG_CONFIG_HOME" || {
+    cp -vr "$baseSetPicomDir"/configDir/picom  "$picomCfgDir" || {
 
-  errorCode=$?
 
-  errXMsg "'picom' Directory Copy Failed" "$MIKE_Setup_Scripts/configDir/picom -> $XDG_CONFIG_HOME" "Script: configPicom.sh" "Error Code: $errorCode"
+      picomErrCode=$?
 
-  return $errorCode
+      errXMsg "'picom' Directory Copy Failed" "$baseSetPicomDir/configDir/picom -> $baseSetPicomDir/configDir/picom" "Script: configPicom.sh" "Error Code: $picomErrCode"
 
- }
+      return $picomErrCode
 
-return 0
+     }
+
+  return 0
+
+}
+
+msgNotify "Creating 'picom' config directory - if necessary." &&
+makeDirIfNotExist "$picomCfgDir" &&
+msgNotify "Copying 'picom' config files to config directory." &&
+copyPicomCfgFileToLocal &&
+changeToDir "$picomStartDir" &&
+successMsg "Configured 'picom' configuration files in Directory:" "$picomCfgDir" "Script: configPicom.sh" || {
+  picomErrCode=$?
+  errXMsg "Script File configPicom.sh  Execution Failed" "Error Code $picomErrCode" "Script: configPicom.sh"
+}
